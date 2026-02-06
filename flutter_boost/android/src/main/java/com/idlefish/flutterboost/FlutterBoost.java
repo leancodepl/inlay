1d3e00f5f13a5eafb895aefe57903491d2f49449
Marcin Chudy <marcin.chudy@leancode.pl>
2026-02-06 12:05:02 +0100

    Fix Android for Flutter 3.32 and higher

--- flutter_boost/android/src/main/java/com/idlefish/flutterboost/FlutterBoost.java
+++ flutter_boost/android/src/main/java/com/idlefish/flutterboost/FlutterBoost.java
@@ -15,11 +15,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import io.flutter.FlutterInjector;
 import io.flutter.embedding.android.FlutterEngineProvider;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.FlutterEngineCache;
 import io.flutter.embedding.engine.dart.DartExecutor;
-import io.flutter.view.FlutterMain;
 
 public class FlutterBoost {
     public static final String ENGINE_ID = "flutter_boost_default_engine";
@@ -57,6 +57,7 @@ public class FlutterBoost {
     }
 
     public void setup(Application application, FlutterBoostDelegate delegate, Callback callback, FlutterBoostSetupOptions options) {
+        FlutterInjector.instance().flutterLoader().startInitialization(application);
         if (options == null) {
             options = FlutterBoostSetupOptions.createDefault();
         }
@@ -86,7 +87,7 @@ public class FlutterBoost {
             // Pre-warm the cached FlutterEngine.
             engine.getNavigationChannel().setInitialRoute(options.initialRoute());
             engine.getDartExecutor().executeDartEntrypoint(new DartExecutor.DartEntrypoint(
-                    FlutterMain.findAppBundlePath(), options.dartEntrypoint()), options.dartEntrypointArgs());
+                FlutterInjector.instance().flutterLoader().findAppBundlePath(), options.dartEntrypoint()), options.dartEntrypointArgs());
         }
         if (callback != null) callback.onStart(engine);
 
