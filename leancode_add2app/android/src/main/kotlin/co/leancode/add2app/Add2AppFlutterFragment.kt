ac62cef9bc82da8166516b61b3ceb06e74efed30
Damian Cudzik <damian.cudzik@leancode.pl>
2026-02-12 16:20:36 +0100

    Add composable Add2AppFlutterScreen with example

--- leancode_add2app/android/src/main/kotlin/co/leancode/add2app/Add2AppFlutterFragment.kt
+++ leancode_add2app/android/src/main/kotlin/co/leancode/add2app/Add2AppFlutterFragment.kt
@@ -1,5 +1,6 @@
 package co.leancode.add2app
 
+import androidx.activity.ComponentActivity
 import io.flutter.embedding.android.FlutterFragment
 import io.flutter.embedding.engine.FlutterEngine
 
@@ -15,18 +16,45 @@ import io.flutter.embedding.engine.FlutterEngine
  * engine group configuration, or build manually via
  * [FlutterFragment.NewEngineInGroupFragmentBuilder].
  *
- * The Dart entrypoint and initial route are configured via the Fragment arguments
- * (Bundle) set by the builder.
+ * ### Back-dispatcher mode
+ *
+ * When the [ARG_USE_BACK_DISPATCHER] argument is `true` (set automatically by
+ * [Add2AppFlutterScreen]), Flutter's `pop()` calls
+ * `onBackPressedDispatcher.onBackPressed()` instead of `activity.finish()`.
+ * This integrates correctly with Compose Navigation, Fragment back stacks,
+ * and any other `OnBackPressedCallback` consumers.
  */
 class Add2AppFlutterFragment : FlutterFragment() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        Add2AppNavigator.configureEngine(flutterEngine, requireActivity())
+
+        val useBackDispatcher =
+            arguments?.getBoolean(ARG_USE_BACK_DISPATCHER, false) ?: false
+
+        if (useBackDispatcher) {
+            val componentActivity = requireActivity() as ComponentActivity
+            Add2AppNavigator.configureEngine(flutterEngine, requireActivity()) {
+                componentActivity.onBackPressedDispatcher.onBackPressed()
+            }
+        } else {
+            Add2AppNavigator.configureEngine(flutterEngine, requireActivity())
+        }
     }
 
     override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
         super.cleanUpFlutterEngine(flutterEngine)
         Add2AppNavigator.cleanUpEngine(flutterEngine)
+    }
+
+    companion object {
+        /**
+         * Bundle key that switches the fragment to *back-dispatcher* mode.
+         *
+         * When `true`, Flutter's `pop()` invokes
+         * `ComponentActivity.onBackPressedDispatcher.onBackPressed()` instead
+         * of finishing the hosting Activity.
+         */
+        internal const val ARG_USE_BACK_DISPATCHER = "add2app_use_back_dispatcher"
     }
 }
